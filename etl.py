8b81ebf2749f7ca4391fd7304d75621dc7b5377a
Ashmaawyy <al_ashmawy@outlook.com>
2022-09-21 20:27:32 +0200

    Better code structure : )

--- etl.py
+++ etl.py
@@ -14,34 +14,42 @@ os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
-    spark = SparkSession \
+    spark_session = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
         .getOrCreate()
-    return spark
+    return spark_session
 
 
-def process_song_data(spark, input_data, output_data):
+def create_song_data_df(spark_session, input_data, output_data):
     # get filepath to song data file
     song_data = 
     
     # read song data file
-    df = 
+    df =
 
+def write_songs_table():
     # extract columns to create songs table
     songs_table = 
     
     # write songs table to parquet files partitioned by year and artist
     songs_table
 
+def write_artists_table():
     # extract columns to create artists table
     artists_table = 
     
     # write artists table to parquet files
-    artists_table
+    artists_table 
+
+def process_songs_data(spark_session, input_data, output_data):
+    
+    create_song_data_df(spark_session, input_data, output_data)
+    write_songs_table()
+    write_artists_table()
 
 
-def process_log_data(spark, input_data, output_data):
+def create_log_data_df(spark_session, input_data, output_data):
     # get filepath to log data file
     log_data =
 
@@ -51,12 +59,14 @@ def process_log_data(spark, input_data, output_data):
     # filter by actions for song plays
     df = 
 
+def write_users_table():
     # extract columns for users table    
-    artists_table = 
+    users_table = 
     
     # write users table to parquet files
-    artists_table
+    users_table
 
+def write_time_table():
     # create timestamp column from original timestamp column
     get_timestamp = udf()
     df = 
@@ -71,6 +81,7 @@ def process_log_data(spark, input_data, output_data):
     # write time table to parquet files partitioned by year and month
     time_table
 
+def write_songplays_table():
     # read in song data to use for songplays table
     song_df = 
 
@@ -81,13 +92,22 @@ def process_log_data(spark, input_data, output_data):
     songplays_table
 
 
+def process_logs_data(spark_session, input_data, output_data):
+
+    create_log_data_df(spark_session, input_data, output_data)
+    write_users_table()
+    write_time_table()
+    write_songplays_table()
+
+
+
 def main():
-    spark = create_spark_session()
+    spark_session = create_spark_session()
     input_data = "s3a://udacity-dend/"
     output_data = ""
     
-    process_song_data(spark, input_data, output_data)    
-    process_log_data(spark, input_data, output_data)
+    process_songs_data(spark_session, input_data, output_data)    
+    process_logs_data(spark_session, input_data, output_data)
 
 
 if __name__ == "__main__":
